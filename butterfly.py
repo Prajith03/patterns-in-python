c0acaa57f8adc87e113f0e376f4c5597a531d2e2
Prajith PS <Prajithps8903@gmail.com>
2024-03-26 16:27:39 +0530

    Delete butterfly.py

--- butterfly.py
+++
@@ -1,7 +0,0 @@
-def print_butterfly(size):
-    for i in range(1, size + 1):
-        print("*" * i + " " * (size - i) + " " * (size - i) + "*" * i)
-    for i in range(size-1, 0, -1):
-        print("*" * i + " " * (size - i) + " " * (size - i) + "*" * i)
-
-print_butterfly(10)
