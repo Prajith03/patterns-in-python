041baf23c11040f68f6f94fedf623b5921e480d1
Prajith PS <94048804+Prajith03@users.noreply.github.com>
2024-03-26 11:00:36 +0530

    Add files via upload

---
+++ rectangle.py
@@ -0,0 +1,8 @@
+def printrect(l,b):
+    for i in range(l):
+      for j in range(b):
+          print("*",end=" ")
+      print()
+
+l = int(input(" Enter the length : "))
+b = int(input(" Enter the breadth : "))
---
+++ triangle.py
@@ -0,0 +1,5 @@
+def print_right_triangle(rows):
+    for i in range(1, rows + 1):
+        print("*" *i)
+rows = int(input("Enter the number of rows for the right triangle: "))
+
